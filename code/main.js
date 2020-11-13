69b05790fdc551192f46e046c28140bdf00c5520
Amadej <as6768@student.uni-lj.si>
2020-11-13 14:48:04 +0100

    Hello

--- code/main.js
+++ code/main.js
@@ -11,12 +11,10 @@ class Application {
     }
 
     _initGL() {
-        // Try to create a WebGL 2.0 context.
-        // We need both a try-catch and a null check.
         this.gl = null;
         try {
             this.gl = this.canvas.getContext('webgl2', {
-                // options, such as disabling the depth buffer
+
             });
         } catch (error) {
         }
@@ -34,19 +32,14 @@ class Application {
     }
 
     _resize() {
-        // Check for resize on RAF, because elements do not
-        // trigger a resize event. Windows do, but it might
-        // not change the size of the canvas.
         const canvas = this.canvas;
         const gl = this.gl;
 
-        if (canvas.width !== canvas.clientWidth ||
-            canvas.height !== canvas.clientHeight)
-        {
+        if (canvas.width !== canvas.clientWidth || canvas.height !== canvas.clientHeight){
+
             canvas.width = canvas.clientWidth;
             canvas.height = canvas.clientHeight;
 
-            // Change the drawing region to reflect the canvas size.
             gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
 
             this.resize();
@@ -54,15 +47,14 @@ class Application {
     }
 
     start() {
-        // initialization code (including event handler binding)
+
     }
 
     update() {
-        // update code (input, animations, AI ...)
+
     }
 
     render() {
-        // render code (gl API calls)
 
         const gl = this.gl;
         gl.clearColor(0.3, 0.4, 0.9, 1.0);
@@ -70,14 +62,12 @@ class Application {
     }
 
     resize() {
-        // resize code (e.g. update projection matrix)
+
     }
 
 }
 
-// Create a new Application only after the body
-// and the canvas element are created.
 document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.querySelector('canvas');
-const app = new Application(canvas);
+    const app = new Application(canvas);
 });
