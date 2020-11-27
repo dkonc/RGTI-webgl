2a6ec0d670871dde5b3a6487c565a4c74ef253ef
Amadej <as6768@student.uni-lj.si>
2020-11-27 19:37:15 +0100

    Heavy load

--- code/17-game/17-game.js
+++ code/17-game/17-game.js
@@ -13,6 +13,23 @@ let past = false;
 let index = 0; //index modela
 let color = 0; //0 = RED, 1 = BLUE
 let fig = [-1,-1,-1,-1,-1,-1,-1,-1];
+let spawns = [
+    [-2,0,-7.3],
+    [-1.2,0,-7.3],
+    [-2,0,-6.5],
+    [-1.2,0,-6.5],
+    [1.2,0,-7.3],
+    [2,0,-7.3],
+    [1.2,0,-6.5],
+    [2,0,-6.5]
+]
+let keyDelete = 0; //0 = preveri(), 1 = nepreveri
+let met1 = 0;
+let met2 = 0;
+
+//odlocnilne spremenljivke
+let rdecCilj = [0,0,0,0];
+let modriCilj = [0,0,0,0];
 
 //figura1red
 let firstMove1 = true; //true, ce mora biti teleportiran na start
@@ -45,6 +62,7 @@ let pastBoolean7 = false;
 //figura4blue
 let firstMove8 = true;
 let pastBoolean8 = false;
+
 
 class App extends Application {
 
@@ -87,45 +105,90 @@ class App extends Application {
     }
 
     logika(index,color){
-        sprem += 0.01;
+        sprem += 0.05;
         let x = sprem;
         let d = this.arrayDynamic[index].transform;
 
         if(color == 0){
+            if(fig[index] == 13){
+                switch(index) {
+                    case 0:
+                        mat4.fromTranslation(d,[-4.48,0.05,-7.3]);
+                        rdecCilj[0] = 1;
+                        break;
+                    case 1:
+                        mat4.fromTranslation(d,[-3.66,0.05,-7.3]);
+                        rdecCilj[1] = 1;
+                        break;
+                    case 2:
+                        mat4.fromTranslation(d,[-4.48,0.05,-6.5]);
+                        rdecCilj[2] = 1;
+                        break;
+                    case 3:
+                        mat4.fromTranslation(d,[-3.66,0.05,-6.5]);
+                        rdecCilj[3] = 1;
+                        break;
+                    default:
+                        //nothing
+                }
+            }
             if(fig[index] == 12 && x <= 1.1){
-                mat4.fromTranslation(d,[d[12], d[13], d[14]+0.01]);
+                mat4.fromTranslation(d,[d[12], d[13], d[14]+0.05]);
             }
             else if(fig[index] == 11 && x <= 1.1){
-                mat4.fromTranslation(d,[d[12]-0.01, d[13], d[14]]);
+                mat4.fromTranslation(d,[d[12]-0.05, d[13], d[14]]);
             }
             else if(fig[index] >= 4 && fig[index] < 11 && x <= 1.1){
-                mat4.fromTranslation(d,[d[12], d[13], d[14]-0.01]);
+                mat4.fromTranslation(d,[d[12], d[13], d[14]-0.05]);
             }
             else if(fig[index] == 3 && x <= 1.1){
-                mat4.fromTranslation(d,[d[12]+0.01, d[13], d[14]]);
+                mat4.fromTranslation(d,[d[12]+0.05, d[13], d[14]]);
             }
             else if(x <= 1.1){
-                mat4.fromTranslation(d,[d[12], d[13], d[14]+0.01]);
+                mat4.fromTranslation(d,[d[12], d[13], d[14]+0.05]);
             }
             else{
                 return;
             }
         }
         else if(color == 1){
+
+            if(fig[index] == 13){
+                switch(index) {
+                    case 4:
+                        mat4.fromTranslation(d,[3.5,0.05,-7.3]);
+                        modriCilj[0] = 1;
+                        break;
+                    case 5:
+                        mat4.fromTranslation(d,[4.35,0.05,-7.3]);
+                        modriCilj[1] = 1;
+                        break;
+                    case 6:
+                        mat4.fromTranslation(d,[3.5,0.05,-6.5]);
+                        modriCilj[2] = 1;
+                        break;
+                    case 7:
+                        mat4.fromTranslation(d,[4.35,0.05,-6.5]);
+                        modriCilj[3] = 1;
+                        break;
+                    default:
+                    //nothing
+                }
+            }
             if(fig[index] == 12 && x <= 1.1){
-                mat4.fromTranslation(d,[d[12], d[13], d[14]+0.01]);
+                mat4.fromTranslation(d,[d[12], d[13], d[14]+0.05]);
             }
             else if(fig[index] == 11 && x <= 1.1){
-                mat4.fromTranslation(d,[d[12]+0.01, d[13], d[14]]);
+                mat4.fromTranslation(d,[d[12]+0.05, d[13], d[14]]);
             }
             else if(fig[index] >= 4 && fig[index] < 11 && x <= 1.1){
-                mat4.fromTranslation(d,[d[12], d[13], d[14]-0.01]);
+                mat4.fromTranslation(d,[d[12], d[13], d[14]-0.05]);
             }
             else if(fig[index] == 3 && x <= 1.1){
-                mat4.fromTranslation(d,[d[12]-0.01, d[13], d[14]]);
+                mat4.fromTranslation(d,[d[12]-0.05, d[13], d[14]]);
             }
             else if(x <= 1.1){
-                mat4.fromTranslation(d,[d[12], d[13], d[14]+0.01]);
+                mat4.fromTranslation(d,[d[12], d[13], d[14]+0.05]);
             }
             else{
                 return;
@@ -209,8 +272,77 @@ class App extends Application {
             this.physics.update(dt);
         }
 
+        //kocka1
+        document.getElementById("tb").addEventListener("click",function() {
+            met1 = document.getElementById("tb").innerHTML;
+            //console.log(met1);
+        });
+        //kocka2
+        document.getElementById("tb").addEventListener("click",function() {
+            met2 = document.getElementById("tb").innerHTML;
+            //console.log(met2);
+        });
+
+
+        if(rdecCilj[0] == 1 && rdecCilj[1] == 1 && rdecCilj[2] == 1 && rdecCilj[3] == 1){
+            alert("Zmagal je rdeči igralec!");
+        }
+        if(modriCilj[0] == 1 && modriCilj[1] == 1 && modriCilj[2] == 1 && modriCilj[3] == 1){
+            alert("Zmagal je modri igralec!");
+        }
+
         if(this.arrayDynamic){
             let that = this;
+            //eliminiraj r1
+            document.getElementById("elim_r1").addEventListener("click",function() {
+                let d = that.arrayDynamic[0].transform;
+                mat4.fromTranslation(d,spawns[0]);
+                pastBoolean1 = false;
+                fig[0] = -1;
+            });
+            document.getElementById("elim_r2").addEventListener("click",function() {
+                let d = that.arrayDynamic[1].transform;
+                mat4.fromTranslation(d,spawns[1]);
+                pastBoolean2 = false;
+                fig[1] = -1;
+            });
+            document.getElementById("elim_r3").addEventListener("click",function() {
+                let d = that.arrayDynamic[2].transform;
+                mat4.fromTranslation(d,spawns[2]);
+                pastBoolean3 = false;
+                fig[2] = -1;
+            });
+            document.getElementById("elim_r4").addEventListener("click",function() {
+                let d = that.arrayDynamic[3].transform;
+                mat4.fromTranslation(d,spawns[3]);
+                pastBoolean4 = false;
+                fig[3] = -1;
+            });
+            document.getElementById("elim_b1").addEventListener("click",function() {
+                let d = that.arrayDynamic[4].transform;
+                mat4.fromTranslation(d,spawns[4]);
+                pastBoolean5 = false;
+                fig[4] = -1;
+            });
+            document.getElementById("elim_b2").addEventListener("click",function() {
+                let d = that.arrayDynamic[5].transform;
+                mat4.fromTranslation(d,spawns[5]);
+                pastBoolean6 = false;
+                fig[5] = -1;
+            });
+            document.getElementById("elim_b3").addEventListener("click",function() {
+                let d = that.arrayDynamic[6].transform;
+                mat4.fromTranslation(d,spawns[6]);
+                pastBoolean7 = false;
+                fig[6] = -1;
+            });
+            document.getElementById("elim_b4").addEventListener("click",function() {
+                let d = that.arrayDynamic[7].transform;
+                mat4.fromTranslation(d,spawns[7]);
+                pastBoolean8 = false;
+                fig[7] = -1;
+            });
+
             window.onkeypress=function(e){
                 if(e.key == "1"){
                     index = 0;
@@ -316,6 +448,7 @@ class App extends Application {
                 this.logika(index,color);
 
             }
+            //console.log(predvPoz);
 
         }
     }
